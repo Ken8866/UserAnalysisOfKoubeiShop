51dab8d9cb64a5e47b0517a4730dbe018bc9cb6b
weidong <jackel.yan@qq.com>
2019-08-05 17:26:49 +0800

    second

--- src/main/java/org/aura/bigdata/PrestoToStore.java
+++ src/main/java/org/aura/bigdata/PrestoToStore.java
@@ -1,4 +1,20 @@
 package org.aura.bigdata;
 
+import java.sql.*;
+
+
 public class PrestoToStore {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException{
+        Class.forName("com.facebook.presto.jdbc.PrestoDriver");
+        Connection connection = DriverManager.getConnection("jdbc:presto://bigdata:8080/hive/koubei","bigdata",null);  ;
+        Statement stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("select count(user_id), date_format(time_stamp,'%Y-%m-%d') as date, shop_id from user_pay group by date_format(time_stamp,'%Y-%m-%d'), shop_id");
+        while (rs.next()) {
+            System.out.println(rs.getString(1)+',' +rs.getString(2));
+        }
+        rs.close();
+        connection.close();
+
+    }
 }
